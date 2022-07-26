e84cade85df053c85f3abfbd92e431cb113e07b0
MauryaPriyanshu <priyanshu9517@gmail.com>
2022-07-26 21:25:52 +0530

    basic programs

--- basicprograms/src/PrimeNumber.java
+++ basicprograms/src/PrimeNumber.java
@@ -1,2 +1,16 @@
 public class PrimeNumber {
+    public static void main(String[] args){
+        int num = 23;
+        int count = 0;
+        if(num < 2)
+            System.out.println("the number"+num+"is not prime number!");
+        for(int i = 1; i <= num; i++){
+            if(num%1 == 0)
+                count += 1;
+        }
+        if(count > 2)
+            System.out.println("The given number" + num + "is not prime number!!");
+        else
+            System.out.println("The given number" + num+ "is prime number!!");
+    }
 }
