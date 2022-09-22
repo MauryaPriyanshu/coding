351d57acd697ab5ff1f2e2a802a82b0d3e7d85fe
MauryaPriyanshu <priyanshu9517@gmail.com>
2022-09-22 12:18:27 +0530

    basic program

---
+++ datastructure/src/StackArrayImplementation.java
@@ -0,0 +1,19 @@
+//import java.util.ArrayList;
+//
+//public  class StackArrayImplementation {
+//    class Stack {
+//        ArrayList<Integer> list = new ArrayList<>();
+//
+//        public static boolean isEmpty() {
+//            return list.size() == 0;
+//        }
+//
+//        public static void push(int data) {
+//            list.add(data)
+//        }
+//    }
+//     public static void main(String[] args){
+//            List la
+//
+//    }
+//}
