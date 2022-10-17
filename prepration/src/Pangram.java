1044c6f9eda97c9341de7e9b45066ffa9794bd19
MauryaPriyanshu <priyanshu9517@gmail.com>
2022-10-17 22:15:06 +0530

    basic Program

---
+++ prepration/src/Pangram.java
@@ -0,0 +1,25 @@
+public class Pangram {
+    public static void checkIfPangram(String sentence) {
+//        return sentence.chars().distinct().count() == 26;
+        int[] arr = new int[26];
+        for (int i = 0; i < sentence.length(); i++) {
+            arr[sentence.charAt(i) - 'a'] += 1;
+
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (arr[i] == 0) {
+                System.out.println("Sentence is not pangram!");
+            }
+        }
+                System.out.println("Sentence is pangram!");
+
+
+    }
+    public static void main(String []  args){
+        String sentence = "qwerttuiopasdfghjklzxcvbnm";
+        System.out.println("Print the sentence:- " + sentence);
+        checkIfPangram(sentence);
+
+    }
+}
